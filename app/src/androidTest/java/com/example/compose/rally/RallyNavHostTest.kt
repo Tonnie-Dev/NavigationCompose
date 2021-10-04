649c4b6f3a09620c62656e121368a6d68599d03b
Antony <vontonnie@gmail.com>
2021-10-04 08:01:54 +0300

    Create compose testing rule

--- app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -1,2 +1,12 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+
+class RallyNavHostTest{
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    
+}
