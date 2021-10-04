c2c4b4254cd15e94f6f174696c502a1e39cdecd6
Antony <vontonnie@gmail.com>
2021-10-04 08:13:50 +0300

    Create @Before fxn

--- app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -1,12 +1,45 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import org.junit.Assert.fail
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class RallyNavHostTest{
 
     @get:Rule
     val composeTestRule = createComposeRule()
+    lateinit var navContoller:NavHostController
 
-    
+
+
+    /*public fxn annotated with @Test - set the content you wanna test here*/
+
+
+    @Before
+    fun setUpRallyNavHost(){
+
+        composeTestRule.setContent {
+
+            navContoller = rememberNavController()
+            RallyNavHost(navController = navContoller)
+        }
+
+    }
+
+    @Test
+    fun rallyNavHost(){
+
+
+
+        composeTestRule.onNodeWithContentDescription("Overview Screen").assertIsDisplayed()
+        /*ensure your test fails until there is an actual assertion made
+        it serves as a reminder to finish implementing the test*/
+        //fail()
+    }
 }
